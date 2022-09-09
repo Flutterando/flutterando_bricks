f75c7ac4579e4ecc19b77e3029a953d04feeb4d8
jennifer <sjennifersarah@gmail.com>
2022-09-09 16:57:04 -0300

    feat: Bricks for triple and page

---
+++ triple/module/__brick__/triple_brick/{{#snakeCase}}{{triplestorename}}{{/snakeCase}}/{{#snakeCase}}{{triplestorename}}_module{{/snakeCase}}.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_triple/flutter_triple.dart';
+
+class {{triplestorename.pascalCase()}} extends NotifierStore<Exception, int> {
+
+  {{triplestorename.pascalCase()}}() : super(0);
+
+}
