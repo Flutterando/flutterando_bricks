6131b3af76ea75b449d4eca69860d77348f7a42e
jennifer <sjennifersarah@gmail.com>
2022-09-16 14:31:20 -0300

    Fixes: triple removed, service structure fixed

--- triple/__brick__/{{#snakeCase}}{{triplestorename}}{{/snakeCase}}/{{#snakeCase}}{{triplestorename}}_module{{/snakeCase}}.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_triple/flutter_triple.dart';
-
-class {{triplestorename.pascalCase()}} extends NotifierStore<Exception, int> {
-
-  {{triplestorename.pascalCase()}}() : super(0);
-
-}
