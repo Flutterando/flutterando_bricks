b36b2cc91d9d216ece4490b48681e9772ff0caef
Bwolf <bwolfnoob@gmail.com>
2022-09-29 12:57:13 -0300

    Fix Module Brick

--- module/__brick__/lib/app/modules/{{name.snakeCase()}}/{{name}}_module.dart
+++ module/__brick__/lib/app/modules/{{name.snakeCase()}}/{{name}}_module.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'views/pages/{{#snakeCase}}{{name}}_page {{/snakeCase}}.dart';
-import 'views/pages/{{#snakeCase}}{{name}}_store {{/snakeCase}}.dart';
+import 'presenter/pages/{{#snakeCase}}{{name}}_page {{/snakeCase}}.dart';
+import 'presenter/pages/{{#snakeCase}}{{name}}_store {{/snakeCase}}.dart';
 
 class {{#pascalCase}}{{name}} module {{/pascalCase}} extends Module {
   @override
